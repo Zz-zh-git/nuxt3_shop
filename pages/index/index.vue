7b31397aa2b7b8ba3485c0aa09ab59885b5d19b1
Zz丶zh <5135961+zzehong@user.noreply.gitee.com>
2024-05-09 15:48:31 +0800

    删除elementplus库

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,11 +1,12 @@
 <template>
     <div>
-        <el-button>Default</el-button>
+        首页
+        <!-- <el-button>Default</el-button>
         <el-button type="primary">Primary</el-button>
         <el-button type="success">Success</el-button>
         <el-button type="info">Info</el-button>
         <el-button type="warning">Warning</el-button>
-        <el-button type="danger">Danger</el-button>
+        <el-button type="danger">Danger</el-button> -->
     </div>
 </template>
 <script setup lang="ts">
