e7f677f4830bdc0b000739d5ca8a908151c53ce6
Zz丶zh <5135961+zzehong@user.noreply.gitee.com>
2024-05-09 15:31:42 +0800

    测试

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <!-- <div>
+    {{ res }}
+  </div> -->
+  <NuxtLink to="/">首页</NuxtLink>
+  <NuxtLink to="/news">新闻页</NuxtLink>
+  <NuxtPage></NuxtPage>
+</template>
+<script setup lang="ts">
+  // const res = await $fetch('http://localhost:8888/api/private/v1/login',
+  // {
+  //   method: 'POST',
+  //   body: { username: 'admin', password: '123456' }
+  // })
+</script>
---
+++ pages/news/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="news">
+        news
+        <h1>detail</h1>
+    </div>
+</template>
+<script setup lang="ts">
+useSeoMeta({
+    title: '新闻页'
+})
+</script>
+<style lang="less" scoped>
+.news {
+    color: red;
+
+    h1 {
+        color: skyblue;
+    }
+}
+</style>
